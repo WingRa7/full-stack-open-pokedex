0b865d62e5f376e24f49913b4d53200867a059b9
Tom Raine <tomgraine@gmail.com>
2025-11-18 23:50:55 +0800

    Exercise 11.9 complete

---
+++ e2e-tests/pokedex.spec.js
@@ -0,0 +1,19 @@
+const { test, describe, expect } = require('@playwright/test')
+
+describe('Pokedex', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('/')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(
+      page.getByText(
+        'Pokémon and Pokémon character names are trademarks of Nintendo.'
+      )
+    ).toBeVisible()
+  }),
+  test('we can navigate to a pokemon page using a link', async ({ page }) => {
+    await page.goto('/')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await page.getByRole('link', { name: 'bulbasaur' }).click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
+})
