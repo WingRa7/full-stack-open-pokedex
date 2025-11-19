6082d6f62cded08a481e74764929b2bd4e448a56
Tom Raine <tomgraine@gmail.com>
2025-11-19 18:06:33 +0800

    Exercise 11.11 complete

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
 // get the port from env variable
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5001;
 
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`)
-})
+  console.log(`server started on port ${PORT}`);
+});
 
-app.get('/version', (req, res) => {
-  res.send('3') // change this string to ensure a new version deployed
-})
+app.get("/version", (req, res) => {
+  res.send("4"); // change this string to ensure a new version deployed
+});
